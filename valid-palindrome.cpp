a7e42348b0742687c801deb2c583c6a2e1eb50fa
Sahil <51375919+sa1123@users.noreply.github.com>
2023-05-12 12:07:23 +0530

    https://leetcode.com/problems/valid-palindrome/submissions/948858283/

---
+++ valid-palindrome.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/valid-palindrome/submissions/948858283/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+
+        int end = s.size()-1;
+        int start = 0;
+        
+        while(start<end){
+            if(!isalnum(s[start])){
+                start++;
+                continue;
+            }
+            if(!isalnum(s[end])){
+                end--;
+                continue;
+            }
+            cout<<s[start]<<tolower(s[start])<<" "<<s[end]<<tolower(s[end])<<endl;
+            if(tolower(s[start++]) != tolower(s[end--])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
