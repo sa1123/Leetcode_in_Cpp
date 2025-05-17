da1533e32c00d24d9ccb75af2fc2b996b1fe2aef
Sahil <51375919+sa1123@users.noreply.github.com>
2025-05-17 11:31:26 +0530

    209. https://leetcode.com/problems/to-lower-case/submissions/1636105994

--- to-lower-case.cpp
+++ to-lower-case.cpp
@@ -1,2 +1,13 @@
 // https://leetcode.com/problems/to-lower-case/submissions/1636105994
 
+class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] >= 65 && s[i] <= 90){
+                s[i] = s[i]+32;
+            }
+        }
+        return s;
+    }
+};
